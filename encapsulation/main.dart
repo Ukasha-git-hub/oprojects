f4d796867e9a261c013f0ccbb426119aca4fd312
Ukasha-git-hub <ukashakhursheed@gmail.com>
2025-05-30 11:20:28 +0500

    set or get

--- encapsulation/main.dart
+++ encapsulation/main.dart
@@ -1,13 +1,15 @@
 import 'person.dart';
 
 void main() {
-  Person person = Person("ALI", 38, "gulashan");
+  Person person = Person("ALI", 38, "gulashan", "zaid", 1000);
 
   person.displayPersonInfo();
-
+  print(person.fatherName);
   print("person address:${person.accomodation}");
   person.setAge = 20;
   person.setName = "ukasha";
+  person.setSalary = 3000;
+  person.SetFatherName = "zaid";
   print("object name:${person.name}");
   print("object age:${person.aged}");
 }
--- encapsulation/person.dart
+++ encapsulation/person.dart
@@ -2,6 +2,11 @@ class Person {
   String _name;
   int _age;
   String _address;
+  String _fathername;
+  int _salary;
+  //private variables
+  String get fatherName => _fathername;
+  int get Salary => _salary;
   String get name => _name;
   int get aged => _age;
   String get accomodation => _address;
@@ -9,14 +14,24 @@ class Person {
     _name = name;
   }
 
+  set setSalary(int salary) {
+    if (salary > 1000) {
+      _salary = Salary;
+    }
+  }
+set SetFatherName(String fathername){
+  _fathername =fathername;
+}
   set setAge(int age) {
     if (age < 30) {
       _age = age;
     }
   }
 
-  Person(this._name, this._age, this._address);
+  Person(this._name, this._age, this._address, this._fathername, this._salary);
   void displayPersonInfo() {
-    print("Person: $name, age: $aged, address: $accomodation");
+    print(
+      "Person: $name, age: $aged, address: $accomodation, father: $fatherName, salary: $Salary",
+    );
   }
 }
